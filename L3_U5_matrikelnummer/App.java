b6cb7babeecf285be23d413f9167801be8c2a683
phedias <daniel.borgs@noxa.de>
2024-10-17 10:29:28 +0200

    fix: alt simpler solution

--- L3_U5_matrikelnummer/App.java
+++ L3_U5_matrikelnummer/App.java
@@ -11,23 +11,21 @@ public class App {
 			return;
 		}
 		// Alternative falls substring nicht erlaubt:
-		/*
-		char beginning = matrikelnummer.charAt(0);
-		char second = matrikelnummer.charAt(1);
-		if (beginning != '5'
-			&& (second != '1'
-				|| second != '0')
-		) {
-			System.out.println("Matrikelnummer muss mit 50, 51 oder 61 beginnen.");
-			System.exit(1);
-		}
-		if (beginning != '6'
-			&& second != '1'
-		) {
-			System.out.println("Matrikelnummer muss mit 50, 51 oder 61 beginnen.");
-			System.exit(1);
-		}
-		*/
+		// char beginning = matrikelnummer.charAt(0);
+		// char second = matrikelnummer.charAt(1);
+		// if (beginning != '5'
+			// && (second != '1'
+				// || second != '0')
+		// ) {
+			// System.out.println("Matrikelnummer muss mit 50, 51 oder 61 beginnen.");
+			// System.exit(1);
+		// }
+		// if (beginning != '6'
+			// && second != '1'
+		// ) {
+			// System.out.println("Matrikelnummer muss mit 50, 51 oder 61 beginnen.");
+			// System.exit(1);
+		// }
 		String beginning = matrikelnummer.substring(0, 2);
 		switch (beginning) {
 			case "50":
@@ -38,6 +36,12 @@ public class App {
 				System.out.println("Matrikelnummer muss mit 50, 51 oder 61 beginnen.");
 				System.exit(1);
 		}
+		// for (int i = 2; i < 7; i++) {
+			// if (matrikelnummer.charAt(i) < '0' || matrikelnummer.charAt(i) > '9') {
+				// System.out.println("Matrikelnummer darf nur aus Zahlen bestehen.");
+				// return;
+			// }
+		// }
 		// Check if the rest of the string is a number
 		// just parse and catch the error?
 		try {
