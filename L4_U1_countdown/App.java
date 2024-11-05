3c18d5aa71a996f43ae0e9ccb46cf7663c9c1474
phedias <daniel.borgs@noxa.de>
2024-11-05 16:59:51 +0100

    fix: countdown loop

--- L4_U1_countdown/App.java
+++ L4_U1_countdown/App.java
@@ -2,7 +2,7 @@ import java.util.concurrent.TimeUnit;
 
 public class App {
 	public static void main(String[] args) throws InterruptedException {
-		for (int i = 15; i > 0; i--) {
+		for (int i = 15; i >= 0; i--) {
 			System.out.println("Seconds remaining: " + i);
 			// Thread.sleep(1000);
 			// Using this for better readability
