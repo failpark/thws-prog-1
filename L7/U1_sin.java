7c29a53c2629ef2b9fd97abfa8a6e5102ce485a5
phedias <daniel.borgs@noxa.de>
2024-11-05 11:10:09 +0100

    feat: solve sin

---
+++ L7/U1_sin.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class U1_sin {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please input a num: ");
+		int x = input.nextInt();
+		input.close();
+		System.out.println(sin(x));
+	}
+
+	private static double sin(double x) {
+		double zaehler = x;
+		double nenner = 1.0;
+		double summe = x;
+		double summand = 1;
+		for (int i = 3; summand > 1E-5 || summand < -1E-5; i += 2) {
+			zaehler = zaehler * x * x * (-1);
+			nenner = nenner * i * (i - 1);
+			summand = zaehler / nenner;
+			summe += summand;
+		}
+		return summe;
+	}
+}
